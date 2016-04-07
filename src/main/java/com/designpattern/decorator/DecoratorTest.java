07f6ccc040d61278a3c56cd8b289cc6537024264
casimir <casimiryang@163.com>
2016-04-07 10:39:54 +0800

    decorator design pattern updated

--- src/main/java/com/designpattern/decorator/DecoratorTest.java
+++ src/main/java/com/designpattern/decorator/DecoratorTest.java
@@ -1,11 +1,6 @@
 package com.designpattern.decorator;
 
 import java.io.*;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 
 /**
  * Created by casimiryang on 2015/7/13.
@@ -13,7 +8,19 @@ import java.util.List;
 public class DecoratorTest {
 
     public static void main(String[] args){
+       IConponent conponent =  new Decorator_3(new Decorator_2(new Decorator_1(new ConcreteConponent())));
+        conponent.fun();
 
+        //IO 流中装饰模式  组合了缓冲和文件输入的两个功能
+      //  BufferedInputStream b = new BufferedInputStream(new FileInputStream(new File("test.txt")));
+        try {
+            Writer writer = new OutputStreamWriter(new FileOutputStream(new File("a.test")));
+            Writer writer2 =new BufferedWriter(new BufferedWriter(new FileWriter(new File("a.ext"))));
+           Writer writer3 =new BufferedWriter(new BufferedWriter(
+                    new OutputStreamWriter(new FileOutputStream(new File("a.test")))));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
