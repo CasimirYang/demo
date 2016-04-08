99cd802f0a1fb32c8ca3ff027dd17c20e8e6ead9
casimir <casimiryang@163.com>
2016-04-08 14:37:49 +0800

    update strategy

--- src/main/java/com/designpattern/strategy/Client.java
+++ src/main/java/com/designpattern/strategy/Client.java
@@ -8,6 +8,12 @@ package com.designpattern.strategy;
  * 客户端必须知道所有的策略类，并自行决定使用哪一个策略类。
  * 这就意味着客户端必须理解这些算法的区别。换言之，策略模式只适用于客户端知道算法或行为的情况。
  *
+ *
+ * 组成：
+ ●　　环境(Context)角色：持有一个Strategy的引用。
+ ●　　抽象策略(Strategy)角色：这是一个抽象角色，通常由一个接口或抽象类实现。此角色给出所有的具体策略类所需的接口。
+ ●　　具体策略(ConcreteStrategy)角色：包装了相关的算法或行为。
+
  */
 public class Client {
 
