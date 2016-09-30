ae080710647c89ad5c318854437732613fcd9a70
yjh <casimiryang@163.com>
2016-09-30 15:04:05 +0800

    rename package property

--- src/main/java/com/spring/Property/MyProperty.java
+++ src/main/java/com/spring/Property/MyProperty.java
@@ -1,18 +1,15 @@
-package com.spring.Property;
+package com.spring.property;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 /**
  * Created by yjh on 16/9/29.
  */
 @Component
-@PropertySource(value ="classpath:com/spring/Property/quartz.properties")
+@PropertySource(value = "classpath:com/spring/property/quartz.properties")
 public class MyProperty{
 
 //为了支持spring el 表达式
--- src/main/java/com/spring/Property/PropertyTest.java
+++ src/main/java/com/spring/Property/PropertyTest.java
@@ -1,10 +1,8 @@
-package com.spring.Property;
+package com.spring.property;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
