dfa00909592a64d17e89b24009a1e603ff454fc9
yjh <casimiryang@163.com>
2017-03-06 23:23:38 +0800

    temp cache

--- src/main/java/com/concurrent/jdk6/DelayQueueTest.java
+++ src/main/java/com/concurrent/jdk6/DelayQueueTest.java
@@ -13,13 +13,19 @@ import java.util.concurrent.TimeUnit;
 public class DelayQueueTest {
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        DelayQueue delayQueue = new DelayQueue();
-        DelayedDemo delayedDemo = new DelayedDemo(5000); //deplay 设置的不是expire 时间， 设置的是延迟多久才可以执行的时间
-        for (int i = 0; i < 10; i++) {
-            delayQueue.put(delayedDemo);
-        }
+        DelayQueue<DelayedDemo> delayQueue = new DelayQueue();
+        DelayedDemo delayedDemo = new DelayedDemo(10000,1); //deplay 设置的不是expire 时间， 设置的是延迟多久才可以执行的时间
+        delayQueue.put(delayedDemo);
+        DelayedDemo delayedDemo2 = new DelayedDemo(15000,2); //deplay 设置的不是expire 时间， 设置的是延迟多久才可以执行的时间
+        delayQueue.put(delayedDemo2);
+        DelayedDemo delayedDemo3 = new DelayedDemo(5000,3); //deplay 设置的不是expire 时间， 设置的是延迟多久才可以执行的时间
+        delayQueue.put(delayedDemo3);
+        DelayedDemo delayedDemo4 = new DelayedDemo(7000,4); //deplay 设置的不是expire 时间， 设置的是延迟多久才可以执行的时间
+        delayQueue.put(delayedDemo4);
+        DelayedDemo delayedDemo5 = new DelayedDemo(6000,5); //deplay 设置的不是expire 时间， 设置的是延迟多久才可以执行的时间
+        delayQueue.put(delayedDemo5);
         while (true){
-            System.out.println(delayQueue.take()); // 五秒后才可以使用
+            System.out.println(delayQueue.take().getType()); // 五秒后才可以使用
         }
     }
 
@@ -27,18 +33,31 @@ public class DelayQueueTest {
 }
 
 class DelayedDemo implements Delayed{
-    private long delay;
-    public DelayedDemo( long delay) {
-        this.delay = delay+ System.currentTimeMillis();
+    private long expireTime;
+    private long type;
+    public DelayedDemo( long delay,long type) {
+        this.expireTime = delay+ System.currentTimeMillis();
+        this.type=type;
     }
 
     @Override
     public long getDelay(TimeUnit unit) {
-        return unit.convert(delay- System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+        return unit.convert(expireTime- System.currentTimeMillis(), TimeUnit.MILLISECONDS);
     }
 
     @Override
     public int compareTo(Delayed o) {
-        return 0;
+        DelayedDemo that = (DelayedDemo)o;
+        if(this.expireTime > that.expireTime){//过期时刻越靠后，越排在队尾.
+            return 1;
+        }else if(this.expireTime==that.expireTime){
+            return 0;
+        }else{
+            return -1;
+        }
+    }
+
+    public long getType() {
+        return type;
     }
 }
---
+++ src/main/java/com/demo/HashCodeTest.java
@@ -0,0 +1,73 @@
+package com.demo;
+
+import java.util.*;
+
+/**
+ * TreeMap,TreeSet 依赖实现Comparable 的compareTo接口比较;
+ * HashMap,HashSet 依赖hashCode,equals 比较
+ */
+public class HashCodeTest implements Comparable<HashCodeTest>{
+    private String name;
+    private Integer age;
+
+    public HashCodeTest(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        HashCodeTest demo1 = new HashCodeTest("a",11);
+        HashCodeTest demo2 = new HashCodeTest("a",11);
+        System.out.println(demo1.hashCode());
+        System.out.println(demo2.hashCode());
+        TreeMap<HashCodeTest,Integer> map = new TreeMap<>();
+        map.put(demo1,1);
+        map.put(demo2,2);
+        System.out.println(map.keySet()); //TreeMap 依赖实现Comparable 的compareTo接口比较
+
+        HashSet<HashCodeTest> set = new HashSet<>();
+        set.add(new HashCodeTest("a",11));
+        set.add(new HashCodeTest("a",11));
+        System.out.println(set.size()); //HashSet 的key 比较hashcode 和equals
+
+        TreeSet<HashCodeTest> set2 = new TreeSet<>();
+        set2.add(new HashCodeTest("a2",11));
+        set2.add(new HashCodeTest("a2",11));
+        System.out.println(set2.size()); //TreeSet 依赖实现Comparable 的compareTo接口比较
+    }
+    @Override
+    public int hashCode() {
+        if (this == null)
+            return 0;
+        int result = 1;
+        result = 31*(31 * result + (this.name == null ? 0 : this.name.hashCode()))+
+                (this.age == null ? 0 : this.age.hashCode());
+        return result;
+    }
+
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof HashCodeTest) {
+            HashCodeTest demo = (HashCodeTest) obj;
+            return this.name == demo.name && this.age == demo.age;
+        }
+        else return false;
+    }
+
+
+    @Override
+    public int compareTo(HashCodeTest obj) {
+        if (this == obj) {
+            return 0;
+        }
+        HashCodeTest demo = (HashCodeTest) obj;
+        if(this.name == demo.name && this.age == demo.age)
+            return 0;
+        else
+            return -1;
+    }
+}
