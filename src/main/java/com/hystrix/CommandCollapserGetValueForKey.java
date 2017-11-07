b9339f1dd1286b912ecea5adb54fd03fd0c7538c
YJH-PC\yjh <yangjinghua@zqfmedia.com>
2017-11-07 10:41:13 +0800

    CommandCollapserGetValueForKey

--- src/main/java/com/hystrix/CommandCollapserGetValueForKey.java
+++ src/main/java/com/hystrix/CommandCollapserGetValueForKey.java
@@ -1,19 +1,33 @@
 package com.hystrix;
 
 import com.netflix.hystrix.*;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * Created by Lenovo on 2017/9/12.
  */
 public class CommandCollapserGetValueForKey extends HystrixCollapser<List<String>, String, Integer> {
 
+
     private final Integer key;
 
+    private static Setter setter(){
+        //默认key null, 延时10ms，数量 Integer.MAX_VALUE
+        return HystrixCollapser.Setter.withCollapserKey(HystrixCollapserKey.Factory.asKey("private"))
+                .andCollapserPropertiesDefaults(
+                        HystrixCollapserProperties.Setter()
+                                .withMaxRequestsInBatch(2).withMetricsRollingStatisticalWindowInMilliseconds(20)
+                );
+    }
+
     public CommandCollapserGetValueForKey(Integer key) {
+        super(setter());
         this.key = key;
     }
 
@@ -24,7 +38,7 @@ public class CommandCollapserGetValueForKey extends HystrixCollapser<List<String
 
     @Override
     protected HystrixCommand<List<String>> createCommand(final Collection<CollapsedRequest<String, Integer>> requests) {
-        System.out.println("create command");
+        System.out.println("create command"+requests.size());
         return new BatchCommand(requests);
     }
 
@@ -34,6 +48,26 @@ public class CommandCollapserGetValueForKey extends HystrixCollapser<List<String
         for (CollapsedRequest<String, Integer> request : requests) {
             request.setResponse(batchResponse.get(count++));
         }
+    }
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        HystrixRequestContext.initializeContext();
+        CommandCollapserGetValueForKey k1 = new CommandCollapserGetValueForKey(1);
+        CommandCollapserGetValueForKey k2 = new CommandCollapserGetValueForKey(2);
+        CommandCollapserGetValueForKey k3 = new CommandCollapserGetValueForKey(3);
+        Future<String> future = k1.queue();
+        System.out.println("before k2 call");
+        Thread.sleep(2000L);
+        System.out.println("----sleep------");
+        Future<String> future2 = k2.queue();
+        Thread.sleep(2000L);
+        System.out.println("----sleep------");
+        Future<String> future3 = k3.queue();
+        Thread.sleep(2000L);
+        System.out.println("----sleep------");
+        future.get();
+        future2.get();
+        future3.get();
     }
 
     private static final class BatchCommand extends HystrixCommand<List<String>> {
@@ -58,11 +92,14 @@ public class CommandCollapserGetValueForKey extends HystrixCollapser<List<String
             //服务线程池的设置
             HystrixThreadPoolProperties.Setter threadPoolProperties = HystrixThreadPoolProperties.defaultSetter().withCoreSize(2);
 
+
             return Setter.withGroupKey(groupKey)
                     .andCommandKey(commandKey)
                     .andThreadPoolKey(threadPoolKey)
                     .andThreadPoolPropertiesDefaults(threadPoolProperties)
                     .andCommandPropertiesDefaults(commandProperties);
+
+
         }
 
         @Override
@@ -70,6 +107,7 @@ public class CommandCollapserGetValueForKey extends HystrixCollapser<List<String
             System.out.println("run--");
             ArrayList<String> response = new ArrayList<>();
             for (CollapsedRequest<String, Integer> request : requests) {
+                System.out.println("ValueForKey: " + request.getArgument());
                 // artificial response for each argument received in the batch
                 response.add("ValueForKey: " + request.getArgument());
             }
